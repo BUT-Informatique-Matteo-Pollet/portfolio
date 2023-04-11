926e346008969c2c60f4a1ff4788e67aaf55561d
Pollet Matteo <matteo.pollet@etu.umontpellier.fr>
2023-04-11 17:17:22 +0200

    fix multiples

--- js/script.js
+++ js/script.js
@@ -32,3 +32,19 @@ window.onload=function(){
     });
 }
 
+const backToTopButton = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
